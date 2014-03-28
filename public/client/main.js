6eae7e3f994fa1e65aff7cb71355358aa406b5de
Naman Goel <naman34@gmail.com>
2014-03-28 16:49:21 -0700

    Finished requirements until testing

--- public/client/main.js
+++ public/client/main.js
@@ -36,15 +36,26 @@ angular.module('Shortly', ['ngRoute'])
 })
 
 .controller('Create', function($scope, LinkService){
+  var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+
+  $scope.urlIsValid = false;
+  $scope.isValidUrl = function() {
+    $scope.urlIsValid = !! $scope.url.match(rValidUrl);
+  };
+
   $scope.spinnerDisplay = false;
   $scope.linkDisplay = false;
   $scope.submit = function(){
-    $scope.spinnerDisplay = true;
-    LinkService.postLink($scope.url).then(function(link){
-      console.log('Link submitted', link);
-      $scope.link = link.data;
-      $scope.spinnerDisplay = false;
-      $scope.linkDisplay = true;
-    });
+    if($scope.urlIsValid === false) {
+      return;
+    } else {
+      $scope.spinnerDisplay = true;
+      LinkService.postLink($scope.url).then(function(link){
+        console.log('Link submitted', link);
+        $scope.link = link.data;
+        $scope.spinnerDisplay = false;
+        $scope.linkDisplay = true;
+      });
+    }
   };
 });
